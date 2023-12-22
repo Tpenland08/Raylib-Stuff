4ccc9883d1d04259051643ce4eb85e3006901323
Teagan Penland <77027609+Tpenland08@users.noreply.github.com>
2023-12-21 21:25:53 -0500

    Update install.sh

--- Ball/install.sh
+++ Ball/install.sh
@@ -3,5 +3,8 @@
 #
 
 brew install raylib
+mkdir raylib-ball
+cd raylib-ball
+wget https://raw.githubusercontent.com/Tpenland08/Raylib-Stuff/main/Ball/main.c
 eval cc main.c -I/opt/homebrew/Cellar/raylib/5.0/include -L/opt/homebrew/Cellar/raylib/5.0/lib -lraylib -o main
 ./main
